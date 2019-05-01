186701b96ec968d0c92066531b2398009c1fe469
elson lim <elsonlim7@gmail.com>
2019-05-02 00:14:37 +0800

    update docker file to serve using nginx

---
+++ app/build/precache-manifest.37ac3e5892020bc2e39a1080e2af4f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b189b8f0327aa49ff74b96b1949e3258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c98a09808a6be953f8b",
+    "url": "/static/css/main.ec04622b.chunk.css"
+  },
+  {
+    "revision": "1e1a8583bd22b19d8b47",
+    "url": "/static/js/2.e1b101f7.chunk.js"
+  },
+  {
+    "revision": "2c98a09808a6be953f8b",
+    "url": "/static/js/main.d1b24020.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
